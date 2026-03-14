3221727c718e90160bb5c36328c9f220413e64e2
Dar Rehan Rasool <darrehanrasool@gmail.com>
2026-03-14 16:41:58 +0530

    دار ریحان رسول
     دار ریحان رسول

--- code/basic-testing-frontend-Automation/src/parser.js
+++ code/basic-testing-frontend-Automation/src/parser.js
@@ -1,7 +1,5 @@
 export function extractNumbers(formData) {
   const num1Input = formData.get("num1");
   const num2Input = formData.get("num2");
-  console.log(typeof num1Input); // DOM return type string
-  console.log(typeof num2Input); // DOM return type string
   return [num1Input, num2Input];
 }
